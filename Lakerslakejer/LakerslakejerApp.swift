9992797c68ac599866a55f79cc65ee24f76bfe05
mouhammadazroun <yhma22.azromo@folkuniversitetet.nu>
2023-05-15 19:56:47 +0200

    inloggning Views and navigation between views

--- Lakerslakejer/LakerslakejerApp.swift
+++ Lakerslakejer/LakerslakejerApp.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct LakerslakejerApp: App {
     let persistenceController = PersistenceController.shared
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
