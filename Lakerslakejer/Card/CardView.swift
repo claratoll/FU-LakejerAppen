ef378a93042dd3ff4d7e8c9d52fe6092e0bd030b
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2023-05-24 09:49:24 +0200

    fixed Cardview to sync with firestore

--- Lakerslakejer/Card/CardView.swift
+++ Lakerslakejer/Card/CardView.swift
@@ -8,31 +8,39 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    @ObservedObject var cardVM : CardViewModel
+    
     var body: some View {
            ZStack {
+               
                LinearGradient(gradient: Gradient(colors: [.ui.orange,.ui.blue]), startPoint: .topTrailing, endPoint: .bottomLeading)
                
                
                
-                   Text("Name")
+               Text(String(cardVM.name))
                    .position(x:70, y: 30)
+                   .foregroundColor(.white)
                        
-                   Text("Card Number")
+               Text(String(cardVM.memberNr))
                    .position(x:250, y: 30)
+                   .foregroundColor(.white)
                        
-                   Text("Season")
+               Text(Date.now, format: .dateTime.year())
                    .position(x:70, y: 170)
+                   .foregroundColor(.white)
                    
                
                
-           }.frame(width: 350, height: 200)
+           }.onAppear(perform: cardVM.getDetails)
+           .frame(width: 350, height: 200)
                .cornerRadius(12)
           
        }
    }
 
-   struct CardView_Previews: PreviewProvider {
+   /*struct CardView_Previews: PreviewProvider {
        static var previews: some View {
            CardView()
        }
-   }
+    }*/
