0145d490f30345b0c12be39ac86ca09e579408cd
claratoll <clara.toll@gmail.com>
2023-05-24 17:09:20 +0200

    qr scan fixing

--- Lakerslakejer/MenuView.swift
+++ Lakerslakejer/MenuView.swift
@@ -148,7 +148,7 @@ struct ButtonView: View {
                     //                            .cornerRadius(10)
                     //                    }
                     Spacer()
-                    NavigationLink(destination: ProspectsView(filter: .none).onAppear { isCardViewVisible = false } .onDisappear { isCardViewVisible = true}){
+                    NavigationLink(destination: ScannedMembersView().environmentObject(Prospects()).onAppear { isCardViewVisible = false } .onDisappear { isCardViewVisible = true}){
                         Text("ScannerView")
                             .frame(width: 200, height: 50)
                             .background(Color.ui.blue)
