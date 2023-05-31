75dc1c16bd62e7e96020520e4a88cee1e46dd146
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-05-31 13:27:36 +0200

    Navigation och medlemskort rör sig som vi vill det

--- Lakerslakejer/Card/CardView.swift
+++ Lakerslakejer/Card/CardView.swift
@@ -10,38 +10,60 @@ import SwiftUI
 struct CardView: View {
     
     @ObservedObject var cardVM : CardViewModel
-    
+    @State var sideWays : Bool = false
     
     var body: some View {
-           ZStack {
-               
-               LinearGradient(gradient: Gradient(colors: [.ui.orange,.ui.blue]), startPoint: .topTrailing, endPoint: .bottomLeading)
-               
-               
-               
-               Text(String(cardVM.name))
-                   .position(x:70, y: 30)
-                   .foregroundColor(.white)
-                       
-               Text(String(cardVM.memberNr))
-                   .position(x:250, y: 30)
-                   .foregroundColor(.white)
-                       
-               Text(Date.now, format: .dateTime.year())
-                   .position(x:70, y: 170)
-                   .foregroundColor(.white)
+        GeometryReader { geometry in
+            ZStack {
+                if geometry.size.width > geometry.size.height{
+           
+                    LinearGradient(gradient: Gradient(colors: [.ui.orange,.ui.blue]), startPoint: .topTrailing, endPoint: .bottomLeading)
+                        .frame(width: 500, height: 300)
+                        .cornerRadius(12)
+                  
+                    Text(String(cardVM.name))
+                        .position(x:170, y: 70)
+                        .foregroundColor(.white)
+                    
+                    Text(String(cardVM.memberNr))
+                        .position(x:550, y: 70)
+                        .foregroundColor(.white)
+                    
+                    Text(Date.now, format: .dateTime.year())
+                        .position(x:170, y: 290)
+                        .foregroundColor(.white)
+                    
+                }
+                else{
                    
-               
-               
+                    LinearGradient(gradient: Gradient(colors: [.ui.orange,.ui.blue]), startPoint: .topLeading, endPoint: .bottomLeading)
+                        .frame(width: 350, height: 200)
+                        .cornerRadius(12)
+                  
+                    
+                    Text(String(cardVM.name))
+                        .position(x:70, y: 30)
+                        .foregroundColor(.white)
+                    
+                    Text(String(cardVM.memberNr))
+                        .position(x:250, y: 30)
+                        .foregroundColor(.white)
+                    
+                    Text(Date.now, format: .dateTime.year())
+                        .position(x:70, y: 170)
+                        .foregroundColor(.white)
+                    
+                }
+            }
            }.onAppear(perform: cardVM.getDetails)
-      .frame(width: 350, height: 200)
-               .cornerRadius(12)
-          
+              
        }
    }
 
-   /*struct CardView_Previews: PreviewProvider {
+struct CardView_Previews: PreviewProvider {
        static var previews: some View {
-           CardView()
+           CardView(cardVM: CardViewModel()
+           )
+           .previewInterfaceOrientation(.landscapeLeft)
        }
-    }*/
+    }
